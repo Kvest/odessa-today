6e3b43eae57835a0eded0e9e0d7e56da463ecd79
Roman Bielokon <roman_slavyansk@mail.ru>
2014-07-07 23:45:26 +0300

    implemented saving cinemas to content provider

--- src/com/kvest/odessatoday/datamodel/Cinema.java
+++ src/com/kvest/odessatoday/datamodel/Cinema.java
@@ -1,5 +1,8 @@
 package com.kvest.odessatoday.datamodel;
 
+import android.content.ContentValues;
+import com.kvest.odessatoday.provider.TodayProviderContract;
+
 /**
  * Created with IntelliJ IDEA.
  * User: Kvest
@@ -16,4 +19,18 @@ public class Cinema {
     public String description;
     public String worktime;
     public String image;
+
+    public ContentValues getContentValues() {
+        ContentValues values = new ContentValues(8);
+        values.put(TodayProviderContract.Tables.Cinemas.Columns.CINEMA_ID, id);
+        values.put(TodayProviderContract.Tables.Cinemas.Columns.NAME, name);
+        values.put(TodayProviderContract.Tables.Cinemas.Columns.ADDRESS, address);
+        values.put(TodayProviderContract.Tables.Cinemas.Columns.PHONES, phones);
+        values.put(TodayProviderContract.Tables.Cinemas.Columns.TRANSPORT, transport);
+        values.put(TodayProviderContract.Tables.Cinemas.Columns.DESCRIPTION, description);
+        values.put(TodayProviderContract.Tables.Cinemas.Columns.WORK_TIME, worktime);
+        values.put(TodayProviderContract.Tables.Cinemas.Columns.IMAGE, image);
+
+        return values;
+    }
 }
--- src/com/kvest/odessatoday/io/request/GetCinemasRequest.java
+++ src/com/kvest/odessatoday/io/request/GetCinemasRequest.java
@@ -1,14 +1,28 @@
 package com.kvest.odessatoday.io.request;
 
+import android.content.ContentProviderOperation;
+import android.content.Context;
+import android.content.OperationApplicationException;
+import android.os.RemoteException;
+import android.util.Log;
 import com.android.volley.NetworkResponse;
 import com.android.volley.ParseError;
 import com.android.volley.Response;
 import com.android.volley.toolbox.HttpHeaderParser;
 import com.google.gson.Gson;
+import com.kvest.odessatoday.TodayApplication;
+import com.kvest.odessatoday.datamodel.Cinema;
 import com.kvest.odessatoday.io.NetworkContract;
 import com.kvest.odessatoday.io.response.GetCinemasResponse;
+import com.kvest.odessatoday.utils.Constants;
 
 import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.kvest.odessatoday.provider.TodayProviderContract.CINEMAS_URI;
+import static com.kvest.odessatoday.provider.TodayProviderContract.CONTENT_AUTHORITY;
+import static com.kvest.odessatoday.provider.TodayProviderContract.TIMETABLE_URI;
 
 /**
  * Created with IntelliJ IDEA.
@@ -32,14 +46,38 @@ public class GetCinemasRequest extends BaseRequest<GetCinemasResponse> {
             GetCinemasResponse cinemasResponse = gson.fromJson(json, GetCinemasResponse.class);
 
             //save data
-            //TODO
-//            if (getFilmsResponse.isSuccessful()) {
-//                saveFilms(getFilmsResponse.data.films);
-//            }
+            if (cinemasResponse.isSuccessful()) {
+                saveCinemas(cinemasResponse.data.cinemas);
+            }
 
             return Response.success(cinemasResponse, HttpHeaderParser.parseCacheHeaders(response));
         } catch (UnsupportedEncodingException e) {
             return Response.error(new ParseError(e));
+        }
+    }
+
+    private void saveCinemas(List<Cinema> cinemas) {
+        ArrayList<ContentProviderOperation> operations = new ArrayList<ContentProviderOperation>(cinemas.size() + 1);
+
+        //delete cinemas
+        ContentProviderOperation deleteOperation = ContentProviderOperation.newDelete(CINEMAS_URI).build();
+        operations.add(deleteOperation);
+
+        for (Cinema cinema : cinemas) {
+            //insert film
+            operations.add(ContentProviderOperation.newInsert(CINEMAS_URI).withValues(cinema.getContentValues()).build());
+        }
+
+        //apply
+        Context context = TodayApplication.getApplication().getApplicationContext();
+        try {
+            context.getContentResolver().applyBatch(CONTENT_AUTHORITY, operations);
+        }catch (RemoteException re) {
+            Log.e(Constants.TAG, re.getMessage());
+            re.printStackTrace();
+        }catch (OperationApplicationException oae) {
+            Log.e(Constants.TAG, oae.getMessage());
+            oae.printStackTrace();
         }
     }
 }
