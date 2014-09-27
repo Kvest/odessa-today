c359f9396db05551c4dc9a308cc6f3cd998fdd17
Roman Bielokon <roman_slavyansk@mail.ru>
2014-09-27 22:46:27 +0300

    changed action bar arrow

--- src/com/kvest/odessatoday/io/request/GetCinemasRequest.java
+++ src/com/kvest/odessatoday/io/request/GetCinemasRequest.java
@@ -1,28 +1,14 @@
 package com.kvest.odessatoday.io.request;
 
-import android.content.ContentProviderOperation;
-import android.content.Context;
-import android.content.OperationApplicationException;
-import android.os.RemoteException;
-import android.util.Log;
 import com.android.volley.NetworkResponse;
 import com.android.volley.ParseError;
 import com.android.volley.Response;
 import com.android.volley.toolbox.HttpHeaderParser;
 import com.google.gson.Gson;
-import com.kvest.odessatoday.TodayApplication;
-import com.kvest.odessatoday.datamodel.Cinema;
 import com.kvest.odessatoday.io.NetworkContract;
 import com.kvest.odessatoday.io.response.GetCinemasResponse;
-import com.kvest.odessatoday.utils.Constants;
 
 import java.io.UnsupportedEncodingException;
-import java.util.ArrayList;
-import java.util.List;
-
-import static com.kvest.odessatoday.provider.TodayProviderContract.CINEMAS_URI;
-import static com.kvest.odessatoday.provider.TodayProviderContract.CONTENT_AUTHORITY;
-import static com.kvest.odessatoday.provider.TodayProviderContract.TIMETABLE_URI;
 
 /**
  * Created with IntelliJ IDEA.
@@ -36,7 +22,7 @@ public class GetCinemasRequest extends BaseRequest<GetCinemasResponse> {
 
     public GetCinemasRequest(Response.Listener<GetCinemasResponse> listener, Response.ErrorListener errorListener) {
         super(Method.GET, NetworkContract.CinemasRequest.url.toString(), null, listener, errorListener);
-    };
+    }
 
     @Override
     protected Response<GetCinemasResponse> parseNetworkResponse(NetworkResponse response) {
