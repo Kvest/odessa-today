ed9c402156092e40d9b77840a553046bfc9564e1
Roman Bielokon <roman_slavyansk@mail.ru>
2016-06-24 11:24:09 +0300

    implemented request permission for the reading external storage

--- app/src/main/java/com/kvest/odessatoday/ui/fragment/PhotoGalleryFragment.java
+++ app/src/main/java/com/kvest/odessatoday/ui/fragment/PhotoGalleryFragment.java
@@ -1,7 +1,20 @@
 package com.kvest.odessatoday.ui.fragment;
 
+import android.Manifest;
 import android.app.Activity;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -18,6 +31,10 @@ import com.kvest.odessatoday.utils.Utils;
  * Created by roman on 3/18/16.
  */
 public class PhotoGalleryFragment extends BaseFragment implements PhotoGalleryAdapter.OnItemSelectedListener {
+    private final String[] PROJECTION = {MediaStore.Images.Media.DATA};
+    private static final int PICK_PHOTO_REQUEST = 1;
+    private static final int READ_EXTERNAL_STORAGE_PERMISSION = 1;
+
     private static final String ARGUMENT_URLS = "com.kvest.odessatoday.argument.URLS";
     private static final String ARGUMENT_TITLE = "com.kvest.odessatoday.argument.TITLE";
 
@@ -106,8 +123,78 @@ public class PhotoGalleryFragment extends BaseFragment implements PhotoGalleryAd
         }
     }
 
-    private void proposePhoto() {
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        if (requestCode == PICK_PHOTO_REQUEST && resultCode == Activity.RESULT_OK) {
+            sendPhoto(intent.getData());
+        }
+    }
+
+    private void sendPhoto(Uri data) {
+        Context context = getActivity();
+        if (context == null) {
+            return;
+        }
+
+        //show wait dialog
         //TODO
+
+        String photoPath = getPhotoPath(context, data);
+        //TODO
+    }
+
+    private String getPhotoPath(Context context, Uri uri) {
+        Cursor cursor = context.getContentResolver().query(uri, PROJECTION, null, null, null);
+        try {
+            if (cursor.moveToFirst()) {
+                int index = cursor.getColumnIndex(MediaStore.Images.Media.DATA);
+                return cursor.getString(index);
+            }
+        } finally {
+            if (cursor != null) {
+                cursor.close();
+            }
+        }
+
+        return null;
+    }
+
+    private void proposePhoto() {
+        Activity activity = getActivity();
+        if (activity == null) {
+            return;
+        }
+
+        //check read external storage permission
+        if (ContextCompat.checkSelfPermission(activity, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            //ask for permission
+            requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, READ_EXTERNAL_STORAGE_PERMISSION);
+        } else {
+            pickPhoto();
+        }
+    }
+
+    private void pickPhoto() {
+        Intent pickPhoto = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(pickPhoto, PICK_PHOTO_REQUEST);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == READ_EXTERNAL_STORAGE_PERMISSION) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                pickPhoto();
+            } else {
+                if (!ActivityCompat.shouldShowRequestPermissionRationale(getActivity(), Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                    //"never ask again" selected
+                    showErrorSnackbar(getActivity(), R.string.error_propose_photo_no_permission_strict);
+                } else {
+                    showErrorSnackbar(getActivity(), R.string.error_propose_photo_no_permission);
+                }
+            }
+        }
     }
 
     @Override
