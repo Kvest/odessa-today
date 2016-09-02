43a1156c04e36cfb94376c75364a1e7943a1151a
Roman Bielokon <roman_slavyansk@mail.ru>
2016-09-02 17:27:42 +0300

    finished list of the phone codes

--- app/src/main/java/com/kvest/odessatoday/ui/adapter/PhoneCodeAdapter.java
+++ app/src/main/java/com/kvest/odessatoday/ui/adapter/PhoneCodeAdapter.java
@@ -4,6 +4,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 
+import com.kvest.odessatoday.R;
 import com.kvest.odessatoday.datamodel.PhoneCodeMetadata;
 
 /**
@@ -11,55 +12,18 @@ import com.kvest.odessatoday.datamodel.PhoneCodeMetadata;
  */
 public class PhoneCodeAdapter extends BaseAdapter {
     private static final PhoneCodeMetadata[] DATA = new PhoneCodeMetadata[] {
-//        new PhoneCodeMetadata(PhoneCodeMetadata.ID_UKRAINE,),
-//        new PhoneCodeMetadata(PhoneCodeMetadata.ID_RUSSIA,),
-//        new PhoneCodeMetadata(PhoneCodeMetadata.ID_BELARUS,),
-//        new PhoneCodeMetadata(PhoneCodeMetadata.ID_ARMENIA,),
-//        new PhoneCodeMetadata(PhoneCodeMetadata.ID_AZERBAIJAN,),
-//        new PhoneCodeMetadata(PhoneCodeMetadata.ID_KAZAKHSTAN,),
-//        new PhoneCodeMetadata(PhoneCodeMetadata.ID_KYRGYZSTAN,),
-//        new PhoneCodeMetadata(PhoneCodeMetadata.ID_MOLDOVA,),
-//        new PhoneCodeMetadata(PhoneCodeMetadata.ID_TAJIKISTAN,),
-//        new PhoneCodeMetadata(PhoneCodeMetadata.ID_TURKMENISTAN,),
-//        new PhoneCodeMetadata(PhoneCodeMetadata.ID_UZBEKISTAN,),
+        new PhoneCodeMetadata(PhoneCodeMetadata.ID_UKRAINE, R.string.ua, R.drawable.ic_flag_ua, "+380"),
+        new PhoneCodeMetadata(PhoneCodeMetadata.ID_RUSSIA, R.string.ru, R.drawable.ic_flag_ru, "+7"),
+        new PhoneCodeMetadata(PhoneCodeMetadata.ID_BELARUS, R.string.by, R.drawable.ic_flag_by, "+375"),
+        new PhoneCodeMetadata(PhoneCodeMetadata.ID_ARMENIA, R.string.am, R.drawable.ic_flag_am, "+374"),
+        new PhoneCodeMetadata(PhoneCodeMetadata.ID_AZERBAIJAN, R.string.az, R.drawable.ic_flag_az, "+994"),
+        new PhoneCodeMetadata(PhoneCodeMetadata.ID_KAZAKHSTAN, R.string.kz, R.drawable.ic_flag_kz, "+7"),
+        new PhoneCodeMetadata(PhoneCodeMetadata.ID_KYRGYZSTAN, R.string.kg, R.drawable.ic_flag_kg, "+996"),
+        new PhoneCodeMetadata(PhoneCodeMetadata.ID_MOLDOVA, R.string.md, R.drawable.ic_flag_md, "+373"),
+        new PhoneCodeMetadata(PhoneCodeMetadata.ID_TAJIKISTAN, R.string.tj, R.drawable.ic_flag_tj, "+992"),
+        new PhoneCodeMetadata(PhoneCodeMetadata.ID_TURKMENISTAN, R.string.tm, R.drawable.ic_flag_tm, "+993"),
+        new PhoneCodeMetadata(PhoneCodeMetadata.ID_UZBEKISTAN, R.string.uz, R.drawable.ic_flag_uz, "+998")
     };
-//
-//
-//    Версия
-//    1) Не добавляется фотка
-//    2) Добавить код страны
-//    3) Выводить сообщение после покупки билетов
-//    Список ликов на с3 и на лыже
-//    На белой(и на с3 и на лыже) девайсине неверно отображаются радиобатоны
-//
-//    Краши
-//    Иероглифы в Ками + вообще все как то странно с отзывами
-//
-//
-//    https://docs.google.com/spreadsheets/d/1UBq7v9Ipt0vuzBdxNsLhXdu7E8oxTpNirf3T5j2sLhA/edit#gid=0
-//
-//    Добавили комент с рейтингом(в онлайне) - нас вернуло на список коментов. Если еще наз нажать Добавить комент - то можно поставить рейтинг. Если вернуться к событию, и опять открыть коменты - то рейтинг поставить нельзя будет
-//    fix bugs from bug-reports
-//    Проверить что в запросе на список events - все события(не все в выставках)
-//    В детали добавить Pull to refresh?
-//    Проверить overdraw
-//    проанализировать объем трафика
-//    проверить на мемори лики! - что вложенные фрагменты померли
-//    Проыерить что вся работа с БД не в UI потоке
-//
-//    content query --uri content://com.kvest.odessatoday/cinemas --where cinema_id=727
-//
-//    ukraine
-//            russia
-//    belarus
-//            armenia
-//    azerbaijan
-//            kazakhstan
-//    kyrgyzstan
-//            moldova
-//    tajikistan
-//            turkmenistan
-//    uzbekistan
 
     @Override
     public int getCount() {
